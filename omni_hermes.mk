1fae7aff58f273aba65c48ef95e6f29473662d49
bjsiu0617 <bjsiu0617@gmail.com>
2017-03-15 21:00:41 +0800

    edited

--- omni_hermes.mk
+++ omni_hermes.mk
@@ -1,5 +1,11 @@
 $(call inherit-product, device/xiaomi/hermes/device.mk)
 
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
 # 64-bit support
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
